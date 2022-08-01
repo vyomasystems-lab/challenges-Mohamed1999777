d7d5f0fe80b3067ea310f9c6ffeccbb0fcff8be7
Mohamed1999777 <mohammedadel1999777@gmail.com>
2022-08-01 02:03:58 +0000

    fwef

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -146,7 +146,48 @@ def run_test(dut):
 #    }
 #
 #    for x in instructions:
-#        mav_putvalue_src1 = random.randint(0, (2**32)-1)
+#        mav_putvalue_src1 = # level1_design2 Verification
+The verification environment is setup using [Vyoma's UpTickPro](https://vyomasystems.com) provided for the hackathon.
+
+## Verification Environment
+
+The [CoCoTb](https://www.cocotb.org/) based Python test is developed as explained. 
+
+
+The values are assigned to the input port as an array
+
+then a for loop to assign the input [inp_bit] from 0 to the length of array
+    
+    IN = [ 1, 0, 0 ,1 , 1 ,0 , 1, 1 ] 
+    for i in range(0,len(IN)):
+        await RisingEdge(dut.clk)
+        dut.inp_bit.value = IN[i]
+        await FallingEdge(dut.clk)
+
+The assert statement is used for comparing the outut to the expected value.
+
+The following error is seen:
+![level1_2](https://user-images.githubusercontent.com/100050717/182043816-0669eb53-1298-4f0f-b9ea-13b5e1520fc3.PNG)
+
+## Test Scenario *(Important)*
+
+    test_IN = [ 1, 0, 0 ,1 , 1 ,0 , 1, 1 ] 
+    Expected Output: out=1
+    Observed Output in the DUT dut.out=0
+
+Output mismatches for the above inputs proving that there is a design bug
+
+## Design Bug
+
+The design does't support non-sequence overlapping
+
+## Verification Strategy
+
+The input data is randomised all selection lines were tested
+## Is the verification complete ?
+
+Yes
+
 #        mav_putvalue_src2 = random.randint(0, (2**32)-1)
 #        mav_putvalue_src3 = random.randint(0, (2**32)-1)
 #        mav_putvalue_instr = instructions[x]
--- level3_design/test_modules.py
+++ level3_design/test_modules.py
@@ -9,38 +9,73 @@ from cocotb.decorators import coroutine
 from cocotb.triggers import Timer, RisingEdge
 from cocotb.result import TestFailure
 from cocotb.clock import Clock
+# Clock Generation
+@cocotb.coroutine
+def clock_gen(signal):
+    while True:
+        signal.value <= 0
+        yield Timer(1) 
+        signal.value <= 1
+        yield Timer(1)
+
 
 @cocotb.test()
 async def test_modules(dut):
-    """Test for 5 + 10"""
-
-    A = 5
-    B = 10
-
-    # input driving
-    dut.a.value = A
-    dut.b.value = B
-
-    await Timer(2, units='ns')
-
-    assert dut.sum.value == A+B, f"Adder result is incorrect: {dut.X.value} != 15"
-
-
-@cocotb.test()
-async def adder_randomised_test(dut):
     """Test for adding 2 random numbers multiple times"""
 
     for i in range(5):
 
-        A = random.randint(0, 15)
-        B = random.randint(0, 15)
+        A = f'{random.getrandbits(32):=032b}'
+        B = f'{random.getrandbits(32):=032b}'
 
         dut.a.value = A
         dut.b.value = B
 
+        int_A = A[1:17]
+        int_B = B[1:17]
+
+        float_A =  A[17:32]
+        float_B =  B[17:32]
+
+        sign_A= A[0]
+        sign_B= B[0]
+
+        x_int= int(int_A,2)
+        y_int= int(int_B,2)
+
+        x_float = int(float_A,2)/(2**15)
+        y_float = int(float_B,2)/(2**15)
+
+        x = x_int + x_float
+        y = y_int + y_float
+        if sign_A == 1:
+            x = x * -1
+        else:
+            x = x
+
+        if sign_B == 1:
+            y = y * -1
+        else:
+            y = y
+
+        model_result = x + y
+
         await Timer(2, units='ns')
         
-        dut._log.info(f'A={A:05} B={B:05} model={A+B:05} DUT={int(dut.sum.value):05}')
-        assert dut.sum.value == A+B, "Randomised test failed with: {A} + {B} = {SUM}".format(
-            A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
+        dut_32 = dut.c.value
+        sign_dut = dut_32[0]
+        int_dut = dut_32[1:17]
+        float_dut = dut_32 [17:32]
+        dut_res_int = int(int_dut,2)
+        dut_res_float= int(float_dut,2)/(2**15)
+        dut_res = dut_res_int + dut_res_float
+
+        if sign_dut == 1:
+            dut_res = dut_res * -1
+        else:
+            dut_res = dut_res
+
+        dut._log.info(f'A={x} B={y} model={model_result} DUT={dut_res}')
+        assert ddut_res == model_result, "Randomised test failed with: {X} + {Y} = {Z}".format(
+            X=x, Y=y, Z=dut_res)
 
